1ca61c8662390de74dbb02fa1993a3c1672e0daf
Keroro <keroro714@qq.com>
2023-12-27 11:05:30 +0800

    feat(properties): 启用arnights配置

--- arknights/src/main/java/com/keroro/ArknightsApplication.java
+++ arknights/src/main/java/com/keroro/ArknightsApplication.java
@@ -1,9 +1,12 @@
 package com.keroro;
 
+import com.keroro.arknights.config.ArknightsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ ArknightsProperties.class })
 public class ArknightsApplication {
     public static void main(String[] args) {
         SpringApplication.run(ArknightsApplication.class, args);
