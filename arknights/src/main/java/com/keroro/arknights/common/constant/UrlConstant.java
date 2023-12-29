105a8d728def4c6b3d4c4fbc50f6d79b5cdbccff
Keroro <keroro714@qq.com>
2023-12-29 21:13:49 +0800

    fix(arknights): UrlConstant常量改为public

--- arknights/src/main/java/com/keroro/arknights/common/constant/UrlConstant.java
+++ arknights/src/main/java/com/keroro/arknights/common/constant/UrlConstant.java
@@ -9,25 +9,25 @@ public class UrlConstant {
     /**
      * URL前缀：登出/鹰角账户基本信息
      */
-    private final String USER_INFO_URL_PREFIX = "/user/info/v1";
+    public static final String USER_INFO_URL_PREFIX = "/user/info/v1";
 
     /**
      * URL前缀：明日方舟基本信息
      */
-    private final String ARK_USER_INFO_URL_PREFIX = "/u8/user/info/v1";
+    public static final String ARK_USER_INFO_URL_PREFIX = "/u8/user/info/v1";
 
     /**
      * URL前缀：登录
      */
-    private final String USER_AUTH_URL_PREFIX = "/user/auth/v1";
+    public static final String USER_AUTH_URL_PREFIX = "/user/auth/v1";
 
     /**
      * URL前缀：获取登录凭证
      */
-    private final String ACCOUNT_INFO_URL_PREFIX = "/account/info";
+    public static final String ACCOUNT_INFO_URL_PREFIX = "/account/info";
 
     /**
      * URL前缀：卡池记录
      */
-    private final String USER_API_URL_PREFIX = "/user/api";
+    public static final String USER_API_URL_PREFIX = "/user/api";
 }
