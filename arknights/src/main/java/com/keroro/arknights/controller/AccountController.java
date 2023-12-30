612f4203ab3fbb06c6c94b3198fc390e3a1a6c69
Keroro <keroro714@qq.com>
2023-12-30 22:26:49 +0800

    refactor(account): 缩短请求路径
    BREAKING CHANGE: 请求路径改变，无法兼容

--- arknights/src/main/java/com/keroro/arknights/controller/AccountController.java
+++ arknights/src/main/java/com/keroro/arknights/controller/AccountController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Validated
 @RestController
-@RequestMapping("/arknights/account")
+@RequestMapping("/account")
 public class AccountController {
 
     private final ArkAccountService arkAccountService;
