4a7ceb31e01306ac7e708c11a1fce2d71fc8e8a5
Keroro <keroro714@qq.com>
2024-01-03 23:53:59 +0800

    feat: 增加刷新所有账号抽卡记录的定时任务
    每天0点执行

---
+++ arknights/src/main/java/com/keroro/arknights/schedule/UpdateGachaRecordSchedule.java
@@ -0,0 +1,51 @@
+package com.keroro.arknights.schedule;
+
+import com.keroro.arknights.dao.po.ArkAccount;
+import com.keroro.arknights.service.ArkAccountService;
+import com.keroro.arknights.service.ArkGachaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 定时刷新每个账号的抽卡记录
+ * @author wangpeng
+ * @since 2024年01月03日 20:26
+ */
+@Component
+public class UpdateGachaRecordSchedule {
+
+    Logger logger = LoggerFactory.getLogger(UpdateGachaRecordSchedule.class);
+
+    private final ArkAccountService arkAccountService;
+
+    private final ArkGachaService arkGachaService;
+
+    public UpdateGachaRecordSchedule(ArkAccountService arkAccountService, ArkGachaService arkGachaService) {
+        this.arkAccountService = arkAccountService;
+        this.arkGachaService = arkGachaService;
+    }
+
+    /**
+     * 定时刷新每个账号的抽卡记录，每天执行一次
+     */
+    @Scheduled(cron = "0 0 0 * * ? ")
+    public void updateAllAccountGachaRecord() {
+        logger.info("开始任务：刷新抽卡记录");
+
+        List<ArkAccount> accountList = arkAccountService.arkAccountList();
+        logger.info("查询到" + accountList.size() + "个账号");
+
+        for (ArkAccount arkAccount: accountList) {
+            String accountStr = arkAccount.getArkAccount();
+            if (accountStr != null) {
+                int count = arkGachaService.updateGacha(accountStr, 1);
+                logger.info("账号：" + accountStr + "更新成功，新增了" + count + "条记录");
+            }
+        }
+        logger.info("结束任务：刷新抽卡记录");
+    }
+}
