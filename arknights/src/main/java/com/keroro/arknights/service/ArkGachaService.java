3aa9744d1dfc5d7f60b7bbe62c54d766f73ebec3
Keroro <keroro714@qq.com>
2024-01-03 23:41:32 +0800

    perf: 完善刷新抽卡记录的逻辑
    增加没有token时的处理逻辑

--- arknights/src/main/java/com/keroro/arknights/service/ArkGachaService.java
+++ arknights/src/main/java/com/keroro/arknights/service/ArkGachaService.java
@@ -11,6 +11,8 @@ import com.keroro.arknights.common.exception.InterfaceDataException;
 import com.keroro.arknights.config.ArknightsProperties;
 import com.keroro.arknights.dao.GachaRecordComponent;
 import com.keroro.arknights.dao.po.GachaRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,16 +26,21 @@ import java.util.*;
 @Service
 public class ArkGachaService {
 
+    private final Logger logger = LoggerFactory.getLogger(ArkGachaService.class);
+
     private final PhoneTokenCache phoneTokenCache;
 
     private final GachaRecordComponent gachaRecordComponent;
 
     private final ArknightsProperties arknightsProperties;
 
-    public ArkGachaService(PhoneTokenCache phoneTokenCache, GachaRecordComponent gachaRecordComponent, ArknightsProperties arknightsProperties) {
+    private final ArkAccountService arkAccountService;
+
+    public ArkGachaService(PhoneTokenCache phoneTokenCache, GachaRecordComponent gachaRecordComponent, ArknightsProperties arknightsProperties, ArkAccountService arkAccountService) {
         this.phoneTokenCache = phoneTokenCache;
         this.gachaRecordComponent = gachaRecordComponent;
         this.arknightsProperties = arknightsProperties;
+        this.arkAccountService = arkAccountService;
     }
 
     /**
@@ -45,6 +52,13 @@ public class ArkGachaService {
     public int updateGacha(String arkAccount, Integer channelId) {
         // 校验
         Optional<String> token = Optional.ofNullable(phoneTokenCache.getToken(arkAccount));
+        if (token.isEmpty()) {
+            logger.warn("更新抽卡记录时遇到问题，没有此账号的token，尝试进行登录");
+            if (arkAccountService.arkLogin(arkAccount)) {
+                logger.info("登录成功，继续更新抽卡记录");
+                token = Optional.ofNullable(phoneTokenCache.getToken(arkAccount));
+            }
+        }
         token.orElseThrow(() -> new RuntimeException("没有此账号的token"));
 
         // 查询库里此账号最新的记录的抽卡时间，没有则为nul
