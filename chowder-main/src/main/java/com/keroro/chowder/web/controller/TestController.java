011f76a9f536fd2a3cf3115c012807a040b7b85c
Keroro <keroro714@qq.com>
2024-06-19 16:48:16 +0800

    fix(main): 删除swagger注解

--- chowder-main/src/main/java/com/keroro/chowder/web/controller/TestController.java
+++ chowder-main/src/main/java/com/keroro/chowder/web/controller/TestController.java
@@ -1,8 +1,5 @@
 package com.keroro.chowder.web.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +12,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 @RequestMapping("/test")
-@Tag(name = "TestController", description = "springdoc测试controller")
 public class TestController {
 
     @GetMapping("/calAge")
     @ResponseBody
-    @Operation(summary = "计算年龄", description = "描述描述")
-    @Parameter(name = "age", description = "年龄", example = "23")
     public String calAge(String age) {
         return "你的年龄为：" + age;
     }
